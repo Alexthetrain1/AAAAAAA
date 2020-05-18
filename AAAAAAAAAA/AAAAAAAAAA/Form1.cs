bf2a03352d50dbeef5ebce1d7ed491c813bccb4e
Alex <62228704+Alexthetrain1@users.noreply.github.com>
2020-05-19 00:31:18 +0200

    Add files via upload

---
+++ AAAAAAAAAA/AAAAAAAAAA/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace AAAAAAAAAA
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;
+AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
+
+       
+
+        SqlCommand naredba = new SqlCommand("");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'database1DataSet1.Table' table. You can move, or remove it, as needed.
+            this.tableTableAdapter.Fill(this.database1DataSet1.Table);
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table] (
+    [Id]             INT        NOT NULL,
+    [Naziv]          NCHAR (20) NOT NULL,
+    [Vrijeme]        INT        NULL,
+    [Priprema] TEXT       NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
